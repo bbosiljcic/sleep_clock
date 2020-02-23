9140612b548e08c0fc0a7d727e03914ce272dd86
Boris Bosiljcic <b.bosiljcic@gmail.com>
2020-02-23 22:38:28 +0100

    improve the timeout logic

--- src/sleep_clock/sleep_clock.ino
+++ src/sleep_clock/sleep_clock.ino
@@ -1,32 +1,41 @@
 #include "LiquidCrystal.h"
 
+/*
+ * lcd setup
+ */
+
 // This defines the LCD wiring to the DIGITALpins
 const int rs = 2, en = 3, d4 = 4, d5 = 5, d6 = 6, d7 = 7;
 LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
 
 // Digital LCD Constrast setting
-int CONTRAST_PIN = 9;// pin 9 for contrast PWM
-const int CONTRAST_VALUE = 100;// default contrast
+int CONTRAST_PIN = 9; // pin 9 for contrast PWM
+const int CONTRAST_VALUE = 100; // default contrast
 
 // Backglight setting
-const int RESET_TIME = 10 * 5; // seconds * 5 for 200ms loop
+const int RESET_TIME = 10; // in seconds
 int BACKLIGHT_TIMEOUT = RESET_TIME;// set initital timeout
 
 int BACKLIGHT_PIN = 10; // Backlight pin
 const int BACKLIGHT_VALUE = 50; // no more then 7mA !!! default was 120
+bool BACKLIGHT_STATE = true; // true is on; false is off
+
+/*
+ * Timing variables
+ */
 
 // initial Time
-int HOURS=0;
-int MINUTES=0;
-int SECONDS=0;
+int HOURS = 0;
+int MINUTES = 0;
+int SECONDS = 0;
 
-// Minutes for 12 hours
+// some constants we need later for the calculations
 const int TWELVE_HOUR_MINUTES = 720;
 const int TWENTYFOUR_HOUR_MINUTES = 1440;
 
 // day/night switch hours - this defines when the day starts
-int DAY_HOURS=8;
-int DAY_MINUTES=0;
+int DAY_HOURS = 8;
+int DAY_MINUTES = 0;
 
 int CURRENT_STATE = 0; // 0 = night; 1 = day; 
 float CURRENT_PERCENT_PASSED = 0;
@@ -39,27 +48,27 @@ int DAY_SWITCH_BUTTON_VALUE;
 // Pins definition for Buttons
 int HOURS_PIN = 0; // button for hour selection
 int MINUTES_PIN = 1; // button for minute selection
-int DAY_SWITCH_PIN=11; // button for switching mode
+int DAY_SWITCH_PIN = 11; // button for switching mode
 
 // For accurate Time reading, use Arduino Real Time Clock and not just delay()
 static uint32_t last_time, now = 0; // RTC
 
 void handleTimeOverflow() {
-  if(SECONDS == 60){
+  if (SECONDS == 60){
     SECONDS = 0;
     MINUTES = MINUTES + 1;
   }
-  if(MINUTES == 60) {
+  if (MINUTES == 60) {
     MINUTES = 0;
     HOURS = HOURS + 1;
   }
-  if(HOURS == 24) {
+  if (HOURS == 24) {
     HOURS = 0;
   }
-  if(DAY_MINUTES == 60) {
+  if (DAY_MINUTES == 60) {
     DAY_MINUTES = 0;
   }
-  if(DAY_HOURS == 13) {
+  if (DAY_HOURS == 13) {
     // currently only day starts in the first 12 hours are supported
     DAY_HOURS = 0;
   }
@@ -97,6 +106,11 @@ void printDisplay() {
   printTwoDigitValue(DAY_MINUTES);
 }
 
+void resetBacklightTimeout() {
+  BACKLIGHT_TIMEOUT=RESET_TIME;
+  analogWrite(BACKLIGHT_PIN,BACKLIGHT_VALUE);
+}
+
 void handleMinuteCheck() {
   int dayTimeMinutes = DAY_HOURS * 60 + DAY_MINUTES;
   int currentTimeMinutes = HOURS * 60 + MINUTES;
@@ -127,6 +141,7 @@ void setup() {
   // LCD PINS
   analogWrite(CONTRAST_PIN,CONTRAST_VALUE); // Adjust Contrast VO
   analogWrite(BACKLIGHT_PIN,BACKLIGHT_VALUE); // Turn on Backlight
+  BACKLIGHT_STATE = true;
 
   now=millis(); // read RTC initial value  
 
@@ -150,54 +165,40 @@ void loop() {
     MINUTES_BUTTON_VALUE = digitalRead(MINUTES_PIN);
     DAY_SWITCH_BUTTON_VALUE = digitalRead(DAY_SWITCH_PIN);
 
-    //Backlight time out 
-    BACKLIGHT_TIMEOUT--;
-
-    if(BACKLIGHT_TIMEOUT == 0) {
-      analogWrite(BACKLIGHT_PIN,0); // Backlight OFF
-      BACKLIGHT_TIMEOUT++;
-    }
-
-    // Hit any to activate Backlight 
-    if((HOURS_BUTTON_VALUE == LOW | MINUTES_BUTTON_VALUE == LOW | DAY_SWITCH_BUTTON_VALUE == LOW) & (BACKLIGHT_TIMEOUT == 1)) {
-
-
-      BACKLIGHT_TIMEOUT=RESET_TIME;
+    // hit any button while the backlight is off
+    if ((HOURS_BUTTON_VALUE == LOW | MINUTES_BUTTON_VALUE == LOW | DAY_SWITCH_BUTTON_VALUE == LOW) & (BACKLIGHT_STATE == false)) {
+      BACKLIGHT_TIMEOUT = RESET_TIME;
       analogWrite(BACKLIGHT_PIN,BACKLIGHT_VALUE);
+      BACKLIGHT_STATE = true;
       // wait until Button released
       while (HOURS_BUTTON_VALUE == LOW | MINUTES_BUTTON_VALUE == LOW | DAY_SWITCH_BUTTON_VALUE == LOW) {
-        HOURS_BUTTON_VALUE = digitalRead(HOURS_PIN);// Read Buttons
+        HOURS_BUTTON_VALUE = digitalRead(HOURS_PIN);
         MINUTES_BUTTON_VALUE = digitalRead(MINUTES_PIN);
         DAY_SWITCH_BUTTON_VALUE = digitalRead(DAY_SWITCH_PIN);
       }
-    } else { // Process Button 1 or Button 2 when hit while Backlight on 
-
-      if(HOURS_BUTTON_VALUE == LOW) {
+    } else {
+      // when backlight is on we can start processing our buttin inputs
+      if (HOURS_BUTTON_VALUE == LOW) {
         if (DAY_SWITCH_BUTTON_VALUE == LOW) {
           DAY_HOURS = DAY_HOURS + 1;
         } else {
           HOURS = HOURS + 1;
         }
-        BACKLIGHT_TIMEOUT = RESET_TIME;
-        analogWrite(BACKLIGHT_PIN,BACKLIGHT_VALUE);
       }
 
-      if(MINUTES_BUTTON_VALUE == LOW) {
+      if (MINUTES_BUTTON_VALUE == LOW) {
         if (DAY_SWITCH_BUTTON_VALUE == LOW) {
           DAY_MINUTES = DAY_MINUTES + 1;
         } else {
           SECONDS = 0;
           MINUTES = MINUTES + 1;
         }
-        BACKLIGHT_TIMEOUT = RESET_TIME;
-        analogWrite(BACKLIGHT_PIN,BACKLIGHT_VALUE);
       }
-
-
 
       handleTimeOverflow();
 
-      if( HOURS_BUTTON_VALUE == LOW | MINUTES_BUTTON_VALUE == LOW ) { // Update display if time set button pressed
+      if (HOURS_BUTTON_VALUE == LOW | MINUTES_BUTTON_VALUE == LOW) {
+        BACKLIGHT_TIMEOUT = RESET_TIME;
         printDisplay();
       }
     }
@@ -208,6 +209,15 @@ void loop() {
   if (SECONDS == 60) {
     handleMinuteCheck();
   }
+
+  // decriment the backlight timeout time
+  BACKLIGHT_TIMEOUT--;
+  if (BACKLIGHT_TIMEOUT == 0) {
+    // turn the backlight off
+    analogWrite(BACKLIGHT_PIN,0);
+    BACKLIGHT_STATE = false;
+  }
+
   // TODO: remove this line below just for debugging
   handleMinuteCheck();
   handleTimeOverflow();
