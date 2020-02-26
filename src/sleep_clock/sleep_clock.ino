a8fab3b6f0984a828271714234971b34070ca944
Boris Bosiljcic <b.bosiljcic@gmail.com>
2020-02-26 10:07:20 +0100

    fix colors

--- src/sleep_clock/sleep_clock.ino
+++ src/sleep_clock/sleep_clock.ino
@@ -62,16 +62,9 @@ const int LED_PIN = 12;
 const int LED_COUNT = 24;
 
 Adafruit_NeoPixel strip(LED_COUNT, LED_PIN, NEO_GRB + NEO_KHZ800);
+
 uint32_t day = strip.Color(252, 186, 3);
-uint32_t night = strip.Color(61, 3, 252);
-// Argument 1 = Number of pixels in NeoPixel strip
-// Argument 2 = Arduino pin number (most are valid)
-// Argument 3 = Pixel type flags, add together as needed:
-//   NEO_KHZ800  800 KHz bitstream (most NeoPixel products w/WS2812 LEDs)
-//   NEO_KHZ400  400 KHz (classic 'v1' (not v2) FLORA pixels, WS2811 drivers)
-//   NEO_GRB     Pixels are wired for GRB bitstream (most NeoPixel products)
-//   NEO_RGB     Pixels are wired for RGB bitstream (v1 FLORA pixels, not v2)
-//   NEO_RGBW    Pixels are wired for RGBW bitstream (NeoPixel RGBW products)
+uint32_t night = strip.Color(13, 57, 255);
 
 void handleTimeOverflow() {
   if (SECONDS == 60){
